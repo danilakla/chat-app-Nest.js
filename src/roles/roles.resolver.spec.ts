8aee244d211bd6a3665b04eaef2958c5f568d18c
danila <danik.kla@mail.ru>
2022-09-13 17:05:16 +0300

    create roles(test) and add the mockRolesService(create role)

--- src/roles/roles.resolver.spec.ts
+++ src/roles/roles.resolver.spec.ts
@@ -1,19 +1,33 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RolesResolver } from './roles.resolver';
-import { RolesService } from './roles.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {RolesResolver} from './roles.resolver';
+import {RolesService} from './roles.service';
 
 describe('RolesResolver', () => {
-  let resolver: RolesResolver;
+    let resolver: RolesResolver;
+    const mockRolesService = {
+        create:jest.fn(dto=>{
+            return {
+                roleId: Date.now(),
+                ...dto
+            }
+        })
+    }
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [RolesResolver, RolesService],
+        }).overrideProvider(RolesService).useValue(mockRolesService).compile();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesResolver, RolesService],
-    }).compile();
+        resolver = module.get<RolesResolver>(RolesResolver);
+    });
 
-    resolver = module.get<RolesResolver>(RolesResolver);
-  });
+    it('should be defined', () => {
+        expect(resolver).toBeDefined();
+    });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
+    it('create role', () => {
+        expect(resolver.createRole({role: "Admin"})).toEqual({
+            roleId: expect.anything(),
+            role: 'Admin'
+        });
+    });
 });
--- src/roles/roles.service.spec.ts
+++ src/roles/roles.service.spec.ts
@@ -1,18 +1,33 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RolesService } from './roles.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {RolesService} from './roles.service';
+import {getRepositoryToken} from "@nestjs/typeorm";
+import {Roles} from "./entities/role.entity";
 
 describe('RolesService', () => {
-  let service: RolesService;
+    let service: RolesService;
+    const mockRolesRepository = {
+        create:jest.fn().mockImplementation(dto=>dto),
+        save: jest.fn().mockImplementation(dto => Promise.resolve({roleId: 23, role:"User"}))
+    }
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [RolesService, {
+                provide: getRepositoryToken(Roles),
+                useValue: mockRolesRepository
+            }],
+        }).compile();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService],
-    }).compile();
+        service = module.get<RolesService>(RolesService);
+    });
 
-    service = module.get<RolesService>(RolesService);
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('create role in serive', async () => {
+expect(await service.create({role:'Admin'})).toEqual({
+    roleId: 23,
+    role:'Admin'
+})
+    });
 });
